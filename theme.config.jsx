f5e621d472fe001c3af6a07e1cdd4048815a24cd
Joel Mwala <joelmwala7@gmail.com>
2024-05-28 03:46:53 +0200

    make the education direct to multiple docs

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,7 +2,7 @@
 export default {
   logo: <strong>SUNLIGHT</strong>,
   project: {
-    link: 'https://github.com/kazoomarc',
+    link: 'https://github.com/kazoomarc/sunlight-project-site',
   },
   footer: { text: 'footer here' },
   // ... other theme options
