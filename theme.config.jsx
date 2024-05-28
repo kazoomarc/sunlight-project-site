f96ae9c0206ffd03bd217eb747c923b0a39b987a
Joel Mwala <joelmwala7@gmail.com>
2024-05-28 02:25:25 +0200

    initial commit

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  logo: <strong>SUNLIGHT</strong>,
+  project: {
+    link: 'https://github.com/kazoomarc',
+  },
+  footer: { text: 'footer here' },
+  // ... other theme options
+};
